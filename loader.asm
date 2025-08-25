a3d7035f81f003c97cbf2db9d6406d05c1088640
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-25 16:24:11 +0100

    Add files via upload

--- loader.asm
+++ loader.asm
@@ -1,15 +1,17 @@
 bits 16
 ;nasm -f bin -o loader32.com loader.asm
 org 0x100
-boots:
-jmp _start
+init:
+jmp loads
 db '$'
 loopss:
 mov ax,0
 int 0x21
 int 0x20
-_start:
+loads:
+
 ;load into 7000h:0h
+
     mov di,0x80
     mov ax,0
     mov al,[di]
@@ -50,84 +52,51 @@ _start:
     mov dx,0x82
     mov ah,0x9
     int 0x21
-cli
-jmp kernel
-gdt_start:
 
-gdt_null:
-    dd 0x0
-    dd 0x0
-
-gdt_code:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10011010b
-    db 11001111b
-    db 0x0
-
-gdt_data:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10010010b
-    db 11001111b
-    db 0x0
-
-gdt_end:
-
-gdt_descriptor:
-    dw gdt_end - gdt_start
-    dd gdt_start
-
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-kernel:
-    mov ax, 0
-    mov ss, ax
-    mov sp, 0xFFFC
-
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-
-    cli
-    lgdt[gdt_descriptor]
-    mov eax, cr0
-    or eax, 0x1
-    mov cr0, eax
-    jmp CODE_SEG:b32
-
-[bits 32]
-nop 
-
-b32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-
-    mov ebp, 0x90000
-    mov esp, ebp
-;start eax ebx ecx edx to enter on program
+tables:
+    mov si,files
+    mov edi,0x7c00
     mov eax,0
-    mov ebx,0
-    mov ecx,0xf000
-    mov edx,0
-    mov esi,0
-    mov edi,0
+    mov es,ax
+    mov ecx,64
+table2:
     ds
-    call dword 0x70000
-exits:
-halts:
-    jmp halts
-hello:
-db 'hello world.....', 0
-spere:
-signature:
-dw 0xaa55
+    mov eax,[si]
+    es
+    mov [edi],eax
+    inc si
+    inc si
+    inc si
+    inc si
+    inc edi
+    inc edi
+    inc edi
+    inc edi
+    dec ecx
+    cmp ecx,0
+    jnz table2
 
+
+
+    jmp 0:0x7c00
+files:
+incbin "pe.bin"
+
+mov ds,ax
+mov eax,0
+mov ds,ax
+mov es,ax
+mov ebx,0xb8000
+mov ecx,2000
+mov eax,0x6021
+
+loop8:
+    mov [ebx],ax
+    inc ebx
+    inc ebx
+    dec ecx
+    cmp ecx,0
+    jnz loop8
+
+loop9:
+    jmp loop9
